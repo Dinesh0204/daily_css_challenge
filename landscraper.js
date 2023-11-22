de19e3c9228cff81f4ffa0cb8b8b04ccf8b17bae
Dinesh0204 <72650101+Dinesh0204@users.noreply.github.com>
2023-11-22 22:24:18 +0530

    Add files via upload

---
+++ landscraper.js
@@ -0,0 +1,5 @@
+let hamIcon = document.querySelector('.fa-bars');
+let mob_nav = document.querySelector('.mob')
+hamIcon.addEventListener('click', function () {
+    mob_nav.style.display = "flex";
+})
